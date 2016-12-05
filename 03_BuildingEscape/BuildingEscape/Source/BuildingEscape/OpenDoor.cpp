1b6fd81de7e4073eadb23c3fe778193a974ae076
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2016-12-05 16:48:59 -0500

    modifies the gitignore file, creates room, adds non working trigger volume that should open the door

---
+++ 03_BuildingEscape/BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BuildingEscape.h"
+#include "OpenDoor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+    
+    AActor* DoorOwner = GetOwner();
+//    FRotator DoorRotation = DoorOwner->GetActorRotation();
+//    
+//    UE_LOG(LogTemp, Warning, TEXT("%s has this rotation: %s"), *DoorOwner->GetName(), *DoorRotation.ToString());
+    
+    FRotator NewRotation = FRotator(0.f, -60.f, 0.f);
+    
+    DoorOwner->SetActorRotation(NewRotation);
+    
+    UE_LOG(LogTemp, Warning, TEXT("%s has this rotation: %s"), *DoorOwner->GetName(), *NewRotation.ToString());
+
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent( float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction )
+{
+	Super::TickComponent( DeltaTime, TickType, ThisTickFunction );
+
+	// ...
+}
+
